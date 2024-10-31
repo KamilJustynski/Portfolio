528ef1883e92b07c18a001208772469f4b83a93c
Kamil Justynski <kamil.justynski1998@gmail.com>
2024-10-31 15:19:42 +0100

    last fixes

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -16,7 +16,7 @@ export const Work = () => {
                 initial="hidden"
                 whileInView={"show"}
                 viewport={{ once: false, amount: 0.3 }}
-                className="flex flex-col lg:flex-row gap-y-3 justify-between mb-10 lg:mb-0 items-center"
+                className="flex flex-col lg:flex-row gap-y-3 justify-between mb-10 lg:mb-0 lg:gap-x-32 items-center"
             >
               <div className="flex flex-col flex-1 text-center lg:text-left">
                 <h2 className="h2 leading-tight text-accent text-center lg:text-left">
@@ -33,7 +33,7 @@ export const Work = () => {
                   </a>
                 </div>
               </div>
-              <div className="group flex flex-1 mt-5 relative overflow-hidden border-2 border-white/50 rounded-xl max-w-[500px] w-full">
+              <div className="group flex flex-1 relative overflow-hidden border-2 border-white/50 rounded-xl max-w-[700px]">
                 <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
                 <img
                     className="group-hover:scale-125 transition-all duration-500 w-full h-full object-cover"
