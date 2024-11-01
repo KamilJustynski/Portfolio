7c6914e5c9d40f44a3413a63a95064daa981bf17
Kamil Justynski <kamil.justynski1998@gmail.com>
2024-11-01 19:21:56 +0100

    changed number of project completed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,7 +38,7 @@ export const About = () => {
             <div className="flex gap-x-6 lg:gap-x-10 mb-12">
               <div>
                 <div className="text-[40px] font-tertiary text-gradient mb-2">
-                  {inView ? <CountUp start={0} end={7} duration={3} /> : null}
+                  {inView ? <CountUp start={0} end={2} duration={3} /> : null}
                 </div>
                 <div className="font-primary text-sm tracking-[2px]">
                   Project <br />
